aff08d505e2ff0f8ad570f80d042ef34e3d12049
Christian Sporer <12594177+IanStorm@users.noreply.github.com>
2024-04-06 10:23:45 +0200

    Add "go home" functionality

--- src/ui/components/Header/Title.tsx
+++ src/ui/components/Header/Title.tsx
@@ -1,24 +1,32 @@
-import type { SxProps } from "@mui/material";
+import type { BoxProps } from "@mui/material";
 import {
 	Box,
-	Typography,
+	Link as MUILink,
 } from "@mui/material";
 import type { FunctionComponent } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 import { useMeta } from "../../contexts";
+import type { Path } from "../../utils/routes";
 
 export const Title: FunctionComponent = () => {
 	const { meta } = useMeta();
 
-	const sx: SxProps = { flexGrow: 1 };
+	const sx: BoxProps["sx"] = { flexGrow: 1 };
 
 	if (!meta) return <Box sx={sx} />;
 	else return (
-		<Typography
-			sx={sx}
-			variant="h6"
-		>
-			{meta.displayName}
-		</Typography>
+		<Box sx={sx}>
+			<MUILink
+				component={ReactRouterLink}
+				to={"/" satisfies Path}
+
+				color="inherit"
+				underline="hover"
+				variant="h6"
+			>
+				{meta.displayName}
+			</MUILink>
+		</Box>
 	);
 };
